1d65bb6f036f1b22ae37b78e9d7eb3fdf021f530
Behfar Baghovvat <behfar.baghovvat@gmail.com>
2019-02-02 14:21:57 +0330

    Add project files.

---
+++ Integration Constructor and Property/Person.cs
@@ -0,0 +1,110 @@
+﻿
+namespace Integration_Constructor_and_Property
+{
+    class Person
+    {    
+        
+        public Person(int age)
+        {
+            Age = age;
+        }
+
+        /// <summary>
+        /// Fild and Property Age.
+        /// </summary>
+
+        private int age;
+
+        private int Age
+        {
+            get
+            {
+                return age;
+            }
+
+            set
+            {
+                if (value>0 && value<151)
+                {
+                    age = value;
+                }                
+            }
+        }
+
+        /// <summary>
+        /// Fild and Property Full Name.
+        /// </summary>
+
+        private string fullname;
+
+        public string FullName
+        {
+            get
+            {
+                return fullname;
+            }
+            set
+            {
+                if (value==string.Empty)
+                {
+                    fullname = value;
+                }
+
+                if (value==" ")
+                {
+                    fullname=value.Trim();
+
+                }
+                fullname = value;
+            }
+        }
+
+        /// <summary>
+        /// Fild and Property ID.
+        /// </summary>
+
+        private string id;
+
+        public string ID
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+
+        /// <summary>
+        /// Method Show Information.
+        /// </summary>
+
+        public void ShowInfo()
+        {
+            string message =
+                $"My name is { FullName } and { Age } years old. Id code is {ID} ";
+
+            System.Console.WriteLine(message);
+
+            System.Console.WriteLine();
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+    
+}
---
+++ Integration Constructor and Property/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Integration_Constructor_and_Property
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p0 =
+                new Person(age:500)
+                {
+                    ID="SXYW45",
+                    FullName = "Ali Vahidi"
+                };
+
+            p0.ShowInfo();
+
+            Person p1 =
+                new Person(age:35)
+                {
+                    ID="CWQST68",
+                    FullName = "Kamran Lavasani",
+                };
+
+            p1.ShowInfo();
+
+
+
+
+
+
+
+
+
+
+            System.Console.WriteLine();
+            System.Console.ReadLine();
+        }
+    }
+}
